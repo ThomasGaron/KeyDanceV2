6056f069f109e1b4a8950a68d7b79a42dc971194
Thomas Garon <thomasgaron@hotmail.ca>
2025-02-25 00:05:56 -0500

    A faire: Tuto 4_1

---
+++ keydance/src/main/java/key_dance/frontal/vues/VueMenu.java
@@ -0,0 +1,12 @@
+package key_dance.frontal.vues;
+
+import ca.ntro.app.frontend.ViewFx;
+
+public class VueMenu extends ViewFx{
+
+    @Override
+    public void initialize() {
+        
+    }
+
+}
